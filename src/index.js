221404562140a9051e2faad4902674cc14e90a60
pomber <pombopombopombo@gmail.com>
2018-10-30 12:08:26 -0300

    Remove things

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
-import { cachePublisher as cache } from "./spy";
-import { Loader } from "./loader";
 import { showDirector } from "./director";
 import { createResource, lazy } from "./new-cache";
 
-export {
-  cache,
-  Loader,
-  showDirector,
-  utils,
-  createResource as unstable_createResource,
-  lazy
-};
+export { showDirector, createResource as unstable_createResource, lazy };
