ccebb9c8d9516028254beb06f6f5000795f7b376
pomber <pombopombopombo@gmail.com>
2018-10-31 13:31:14 -0300

    Remove debugger

--- examples/suspensify/src/api/index.js
+++ examples/suspensify/src/api/index.js
@@ -1,11 +1,11 @@
-import uniqBy from 'lodash.uniqby';
+import uniqBy from "lodash.uniqby";
 
 import {
   ArtistsJSON,
   ArtistListJSON,
   ArtistAlbumsJSON,
-  ArtistTopTracksJSON,
-} from './data';
+  ArtistTopTracksJSON
+} from "./data";
 
 export function fetchArtistJSON(id) {
   return makeFakeAPICall(`/artists/${id}`, ArtistsJSON(fakeRequestTime)[id]);
@@ -14,14 +14,14 @@ export function fetchArtistJSON(id) {
 export function fetchArtistAlbumsJSON(id) {
   return makeFakeAPICall(
     `/artists/${id}/albums`,
-    uniqBy(ArtistAlbumsJSON(fakeRequestTime)[id], 'name')
+    uniqBy(ArtistAlbumsJSON(fakeRequestTime)[id], "name")
   );
 }
 
 export function fetchArtistTopTracksJSON(id) {
   return makeFakeAPICall(
     `/artists/${id}/top-tracks`,
-    uniqBy(ArtistTopTracksJSON(fakeRequestTime)[id], 'name')
+    uniqBy(ArtistTopTracksJSON(fakeRequestTime)[id], "name")
   );
 }
 
@@ -29,52 +29,56 @@ export function fetchArtistListJSON(query) {
   return makeFakeAPICall(`/artists`, ArtistListJSON);
 }
 
-let fakeRequestTime = 1000;
-let onProgress = () => true;
-
-export function setFakeRequestTime(val) {
-  fakeRequestTime = val;
-}
-
-export function setProgressHandler(handler) {
-  onProgress = handler;
-}
-
-export function setPauseNewRequests(value) {
-  shouldPauseNewRequests = value;
-}
-
-let shouldPauseNewRequests = false;
-let notifiers = {};
-let isPausedUrl = {};
-
-export function setPaused(url, isPaused) {
-  const wasPaused = isPausedUrl[url];
-  isPausedUrl[url] = isPaused;
-  if (isPaused !== wasPaused) {
-    notifiers[url]();
-  }
-}
-
 function makeFakeAPICall(url, result) {
-  let i = 1;
-  return new Promise(resolve => {
-    isPausedUrl[url] = shouldPauseNewRequests;
-    function notify() {
-      if (!isPausedUrl[url]) {
-        i++;
-      }
-      onProgress(url, i, isPausedUrl[url]);
-      if (isPausedUrl[url]) {
-        return;
-      }
-      if (i === 100) {
-        resolve(result);
-      } else {
-        setTimeout(notify, fakeRequestTime / 100);
-      }
-    }
-    notifiers[url] = notify;
-    notify();
-  });
+  return Promise.resolve(result);
 }
+
+let fakeRequestTime = 1000;
+// let onProgress = () => true;
+
+// export function setFakeRequestTime(val) {
+//   fakeRequestTime = val;
+// }
+
+// export function setProgressHandler(handler) {
+//   onProgress = handler;
+// }
+
+// export function setPauseNewRequests(value) {
+//   shouldPauseNewRequests = value;
+// }
+
+// let shouldPauseNewRequests = false;
+// let notifiers = {};
+// let isPausedUrl = {};
+
+// export function setPaused(url, isPaused) {
+//   const wasPaused = isPausedUrl[url];
+//   isPausedUrl[url] = isPaused;
+//   if (isPaused !== wasPaused) {
+//     notifiers[url]();
+//   }
+// }
+
+// function makeFakeAPICall(url, result) {
+//   let i = 1;
+//   return new Promise(resolve => {
+//     isPausedUrl[url] = shouldPauseNewRequests;
+//     function notify() {
+//       if (!isPausedUrl[url]) {
+//         i++;
+//       }
+//       onProgress(url, i, isPausedUrl[url]);
+//       if (isPausedUrl[url]) {
+//         return;
+//       }
+//       if (i === 100) {
+//         resolve(result);
+//       } else {
+//         setTimeout(notify, fakeRequestTime / 100);
+//       }
+//     }
+//     notifiers[url] = notify;
+//     notify();
+//   });
+// }
