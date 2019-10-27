cc4bf37a6f69e44b291eb244eff68d4f07f257db
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-10-27 12:17:24 -0300

    Remove useConcurrentState

--- packages/hitchcock/src/utils.js
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-
-export function useConcurrentState(defaultState) {
-  const [state, setState] = useState(defaultState);
-  const deferSetState = updater => setTimeout(() => setState(updater), 0);
-  return [state, setState, deferSetState];
-}
